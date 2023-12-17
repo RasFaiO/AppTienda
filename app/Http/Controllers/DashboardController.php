fdb5690c95a2cb3241f74e96ff109ceab6389d73
Fabio Lancheros <fylancheros1@misena.edu.co>
2023-12-17 04:45:11 -0500

    sales per month

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,111 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Articulo;
+use App\Models\DetalleVenta;
+use App\Models\Venta;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    // Index
-    public function index(){
-        return view('welcome');
-    }
+// Index
+public function index(){
+    return view('welcome');
+}
     // Dashboard
     public function dashboard(){
-        return view('dashboard');; 
+        // Ventas
+        $ventas = Venta::where('estado','Activo')->get();
+        $ene = $feb = $mar = $abr = $may = $jun = $jul = $ago = $sep = $oct = $nov = $dic = 0;
+        $sell = [];
+        foreach ($ventas as $venta) {
+            $mesVenta = $venta->created_at;
+            $mesVenta = Carbon::create($mesVenta)->month;
+            if ($mesVenta == 1) {
+                $ene = $ene+1;
+            } else {
+                if ($mesVenta == 2) {
+                    $feb = $feb+1;
+                } else {
+                    if ($mesVenta == 3){
+                        $mar = $mar+1;
+                    } else {
+                        if ($mesVenta == 4){
+                            $abr = $abr+1;
+                        } else {
+                            if ($mesVenta == 5){
+                                $may = $may+1;
+                            } else {
+                                if ($mesVenta == 6){
+                                    $jun = $jun+1;
+                                } else {
+                                    if ($mesVenta == 7){
+                                        $jul = $jul+1;
+                                    } else {
+                                        if ($mesVenta == 8){
+                                            $ago = $ago+1;
+                                        } else {
+                                            if ($mesVenta == 9){
+                                                $sep = $sep+1;
+                                            } else {
+                                                if ($mesVenta == 10){
+                                                    $oct = $oct+1;
+                                                } else {
+                                                    if ($mesVenta == 11){
+                                                        $nov = $nov+1;
+                                                    } else {
+                                                        $dic = $dic+1;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+       
+        $sell['ventas'][] = $ene;
+        $sell['ventas'][] = $feb;
+        $sell['ventas'][] = $mar;
+        $sell['ventas'][] = $abr;
+        $sell['ventas'][] = $may;
+        $sell['ventas'][] = $jun;
+        $sell['ventas'][] = $jul;
+        $sell['ventas'][] = $ago;
+        $sell['ventas'][] = $sep;
+        $sell['ventas'][] = $oct;
+        $sell['ventas'][] = $nov;
+        $sell['ventas'][] = $dic;
+
+        $sell['sell'] = json_encode($sell);
+        // return  $sell;
+
+        // Articulos
+        $articulos = Articulo::with('detalleVentas')->get();
+        $article = [];
+        foreach ($articulos as $articulo) {
+            $articulo->cantidadesVendidas = 0;
+            $subtotal = 0;
+            foreach ($articulo->detalleVentas as $venta) {
+                $cantidad = $venta->cantidad;
+                $subtotal = $subtotal+$cantidad;
+            }
+            $articulo->cantidadesVendidas = $subtotal;
+            $article['articulos'][] = [
+                'nombre' => $articulo->nombre,
+                'cantidad' => $articulo->cantidadesVendidas
+            ];
+        }
+        $article['article'] = json_encode($article);
+        return view('dashboard', [
+            'article' => $article['article'],
+            'sell' => $sell['sell']
+        ]);
     }
     //
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,18 +15,121 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <h1 class="px-6 py-6 text-2xl font-medium text-gray-900 dark:text-white">Hello World</h1>
-                {{-- <x-welcome /> --}}
+                <div class="grid gap-2 gap-x-2 grid-cols-2 dark:text-gray-200 text-center">
+                    <figure class="p-2 m-2">
+                        <h2 class="uppercase text-lg">{{ __('sales per month')}}</h2>
+                        <canvas id="ventaMes"></canvas>
+                    </figure>
+                    <figure>
+                        <h2 class="uppercase text-lg">{{ __('purchases per month')}}</h2>
+                        <canvas id="compraMes"></canvas>
+                    </figure>
+                    <figure class="p-2 m-2">
+                        <h2 class="uppercase text-lg">{{ __('Products most selled')}}</h2>
+                        <canvas id="top5"></canvas>
+                    </figure>
+                    <figure>
+                        <h2 class="uppercase text-lg">{{ __('daily sales')}}</h2>
+                        <canvas id="ventasDiarias"></canvas>
+                    </figure>
+                </div>
             </div>
         </div>
     </div>
-    {{-- slot principal --}}
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <h1 class="px-6 py-6 text-2xl font-medium text-gray-900 dark:text-white">Hello World2</h1>
-                {{-- <x-welcome /> --}}
-            </div>
-        </div>
-    </div>
+    @section('js')
+        <script>
+
+            
+            const night = window.matchMedia('(prefers-color-scheme: dark)');
+            if (night.matches){
+                // console.log('dark');
+                Chart.defaults.color = '#fff';
+            } else {
+                // console.log('no dark');
+                Chart.defaults.color = '#000';
+            }
+            
+            function getDataColors(opacity){
+                const colors = ['#7448c2','#21c0d7','#d99e2b','#cd3a81','#9c99cc','#e14eca','#ff0000','#ffffff','#d6ff00','#000fff'];
+                return colors.map(color => opacity ? `${color + opacity}` : color);
+            }
+
+            const articleData = JSON.parse(`<?php echo $article; ?>`);
+            const sellData = JSON.parse(`<?php echo $sell; ?>`);
+
+            function printCharts(){
+                productos();
+                ventas();
+            }
+            
+            function ventas() {
+                const data = {
+                    labels: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+                    datasets: [{
+                        label: '{{__('Sales amount')}}',
+                        data: sellData.ventas,
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.5
+                    }]
+                };
+                new Chart('ventaMes',{
+                    type: 'line', 
+                    data,
+                });
+            }
+
+            function productos(){
+                topFive();
+                const data = {
+                    labels: top_label,
+                    datasets: [{
+                        data: top_data,
+                        borderColor: getDataColors(),
+                        backgroundColor: getDataColors(20)
+                    }]
+                }
+                const options = {
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                            align: 'center',
+                            labels: {
+                                font: {
+                                    size: '15'
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                new Chart('top5', {
+                    type: 'doughnut', 
+                    data, 
+                    options,
+                });
+            }
+
+            function topFive(){
+                // con sort((a,b)) estamos indicando que nos organice el arreglo de mayor a menor cuando retornamos b-a
+                const max = articleData.articulos.sort((a,b) => {
+                    return b['cantidad']-a['cantidad'];
+                });
+                contador = 0;
+                top_label = [];
+                top_data = [];
+                max.forEach(element => {
+                    if (contador < 5) {
+                        top_label.push(element.nombre);
+                        top_data.push(element.cantidad);
+                    }
+                    contador++;
+                });
+            }
+    
+            printCharts();
+    
+            
+        </script>
+    @endsection
 </x-app-layout>
